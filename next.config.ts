5695a3e6f72476df670cb8ee49a59349d0cf3fe6
Kevin8775 <kevinchavez8775@gmail.com>
2026-06-03 09:41:10 -0600

    fixing something again

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.GH_PAGES === "true" ? "/ELC-Las-Flores" : undefined,
+  assetPrefix: process.env.GH_PAGES === "true" ? "/ELC-Las-Flores/" : undefined,
+  images: {
+    unoptimized: true,
+  },
   async rewrites() {
     return [
       {
